612df4b39e630d0cec21fd679d0c9c0949997a40
bottero <alexis.bottero@gmail.com>
2015-10-29 17:07:16 +0100

    Add an example with layers parameterization

---
+++ examples/HomogeneousSyntheticLayersParameterization/run_that_example.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Script that run an example of the use of that program 
+# It needs to be run from "examples/" directory
+#
+###################################################
+
+# Recompile all ('yes' or 'no' or 'justWhatHasChanged')
+MAKEALL='yes'
+# MPI ('yes' or 'no')
+MPI='no'
+# number of processes
+NPROC=1
+
+##################################################
+
+cp configExample.cfg ../../config.cfg
+cd ../../
+
+# Compiles executables in root directory
+if [ $MAKEALL == 'yes' ]; then 
+  echo
+  echo "Compilation and linking in progress ..."
+  echo
+  make clean
+  if [ $MPI == 'yes' ]; then 
+    make PAR=yes
+  else
+    make
+  fi
+  echo
+  echo "Compilation and linking done !"
+  echo
+fi
+
+if [ $MAKEALL == 'justWhatHasChanged' ]; then 
+  echo
+  echo "Compilation and linking in progress ..."
+  echo
+  if [ $MPI == 'yes' ]; then 
+    make PAR=yes
+  else
+    make
+  fi
+  echo
+  echo "Compilation and linking done !"
+  echo
+fi
+
+# Runs program
+echo
+echo "Starting Program..."
+echo
+if [ $MPI == 'yes' ]; then 
+  mpiexec -np $NPROC ./bin/RealisticDataMCMC # or mpirun -n $NPROC
+else
+  ./bin/RealisticDataMCMC
+fi
+
+cd examples/HomogeneousSynthetic/
+
+echo
+echo "Done!"
+echo
+echo "See results in directory: OUTPUT_FILES/ using the tools in utils"
+date
+
+
